ee36bd90860c65cff71cb42fb3111d968b500770
yangle <yangle@luojilab.com>
2021-12-30 18:15:38 +0800

    first triangle

---
+++ main.cpp
@@ -0,0 +1,175 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// GLSL语言, 顶点着色器, 定义顶点位置
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+// GLSL语言, 片段着色器, 片段颜色
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+int main()
+{
+    // 初始化和配置
+    // ----------
+    // 初始化
+    glfwInit();
+    // 这里基于OpenGL版本3.4, 将主版本和次版本都设置为3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // 我们同样明确告诉GLFW我们使用的是核心模式(Core-profile)。明确告诉GLFW我们需要使用核心模式意味着我们只能使用OpenGL功能的一个子集（没有我们已不再需要的向后兼容特性
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // 如果使用的是Mac OS X系统，你还需要加下面这行代码到你的初始化代码中这些配置才能起作用
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // 创建窗口
+    // -------
+    // 创建窗口对象, 参数分别是长、宽、名称, 后面两个参数暂时忽略
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    // 设置这个窗口为上下文
+    glfwMakeContextCurrent(window);
+    // 回调函数, 注册这个函数，告诉GLFW我们希望每当窗口调整大小的时候调用这个函数
+    // 这个函数将窗口大小和视口大小保持一致
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // 调用OpenGL函数之前需要初始化glad2
+    // ------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // 创建和编译着色器
+    // ------------
+    // 创建顶点着色器
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // 检查编译错误
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // 创建片段着色器
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // 检查编译错误
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // 创建着色程序, 并关联上面两个着色器
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // 检查关联错误
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    // 释放着色器
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // 创建顶点数据
+    // -----------
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+    // 顶点缓冲对象, 顶点数组对象
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    // 绑定顶点数组
+    glBindVertexArray(VAO);
+    // 缓冲绑定到VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // 将顶点数据复制到缓冲区供使用
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // 解析顶点属性
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    // 启用顶点属性
+    glEnableVertexAttribArray(0);
+    // 解绑
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // 保持窗口打开, 接受用户输入, 不断绘制
+    // -------------------------------
+    while(!glfwWindowShouldClose(window))
+    {
+        // 接受键盘输入
+        processInput(window);
+
+        // 渲染指令
+        // -------
+        // 清空颜色缓冲并填充为深蓝绿色
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // 绘制三角形
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        // 第二个参数是顶点数组的起始索引, 第二个是绘制多少个顶点
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // 将缓冲区的像素颜色值绘制到窗口
+        glfwSwapBuffers(window);
+        // 检查有没有触发事件
+        glfwPollEvents();    
+    }
+
+    // 释放资源
+    glfwTerminate();
+    return 0;
+}
+
+// 回调函数, 窗口大小改变时, 创建渲染窗口
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // 渲染窗口的大小, glViewport函数前两个参数控制窗口左下角的位置。第三个和第四个参数控制渲染窗口的宽度和高度（像素）
+    // 图形学里的viewport视口
+    glViewport(0, 0, width, height);
+}
+
+// 接受ESC键退出
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
