ecbbf773b9b3f71ab0b92605c29622203a35c5a0
yangle <yangle@luojilab.com>
2021-12-31 22:06:57 +0800

    vertex color

--- main.cpp
+++ main.cpp
@@ -14,19 +14,20 @@ const unsigned int SCR_HEIGHT = 600;
 // GLSL语言, 顶点着色器, 定义顶点位置
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
-    "out vec4 vertexColor;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos, 1.0);\n"
-    "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+    "   ourColor = aColor;\n"
     "}\0";
 // GLSL语言, 片段着色器, 输出片段颜色
 const char *fragmentShaderSource = "#version 330 core\n"
-    "uniform vec4 ourColor;\n"
+    "in vec3 ourColor;\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = ourColor;\n"
+    "   FragColor = vec4(ourColor, 1.0);\n"
     "}\n\0";
 
 int main()
@@ -112,10 +113,10 @@ int main()
     // -----------
     // 4个顶点坐标和三角形
     float vertices[] = {
-         0.5f, 0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f
+         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+        -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f
     };
     unsigned int indices[] = {
         0, 1, 3, // 第一个三角形
@@ -134,10 +135,14 @@ int main()
     // 绑定索引缓冲, 复制数据
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    // 解析顶点属性
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
-    // 启用顶点属性
+    // 解析顶点位置属性
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
+    // 启用顶点位置属性
     glEnableVertexAttribArray(0);
+    // 解析顶点颜色属性
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+    // 启用顶点颜色属性
+    glEnableVertexAttribArray(1);
     // 解绑
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
@@ -157,12 +162,6 @@ int main()
 
         // 绘制三角形
         glUseProgram(shaderProgram);
-
-        float timeValue = glfwGetTime();
-        float greenValue = sin(timeValue) / 2.0f + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-
         glBindVertexArray(VAO);
         // 第二个参数是绘制多少个顶点
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
