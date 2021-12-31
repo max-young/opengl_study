b4813ea12bce4dce95f498ea16bd197676aecf2b
yangle <yangle@luojilab.com>
2021-12-31 09:33:01 +0800

    uniform color

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <math.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -13,16 +14,19 @@ const unsigned int SCR_HEIGHT = 600;
 // GLSL语言, 顶点着色器, 定义顶点位置
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
     "}\0";
-// GLSL语言, 片段着色器, 片段颜色
+// GLSL语言, 片段着色器, 输出片段颜色
 const char *fragmentShaderSource = "#version 330 core\n"
+    "uniform vec4 ourColor;\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = ourColor;\n"
     "}\n\0";
 
 int main()
@@ -153,6 +157,12 @@ int main()
 
         // 绘制三角形
         glUseProgram(shaderProgram);
+
+        float timeValue = glfwGetTime();
+        float greenValue = sin(timeValue) / 2.0f + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
         glBindVertexArray(VAO);
         // 第二个参数是绘制多少个顶点
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
