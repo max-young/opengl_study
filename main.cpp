7dca6f1ffc8c86d77c8b8c99e01e762538689a0f
yangle <yangle@luojilab.com>
2022-01-07 16:28:36 +0800

    灯光衰减

--- main.cpp
+++ main.cpp
@@ -167,6 +167,19 @@ int main()
   lightShader.setInt("material.diffuse", 0);
   lightShader.setInt("material.specular", 1);
 
+  glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f), 
+    glm::vec3( 2.0f,  5.0f, -15.0f), 
+    glm::vec3(-1.5f, -2.2f, -2.5f),  
+    glm::vec3(-3.8f, -2.0f, -12.3f),  
+    glm::vec3( 2.4f, -0.4f, -3.5f),  
+    glm::vec3(-1.7f,  3.0f, -7.5f),  
+    glm::vec3( 1.3f, -2.0f, -2.5f),  
+    glm::vec3( 1.5f,  2.0f, -2.5f), 
+    glm::vec3( 1.5f,  0.2f, -1.5f), 
+    glm::vec3(-1.3f,  1.0f, -1.5f)  
+  };
+
   // 保持窗口打开, 接受用户输入, 不断绘制
   // ---------------------------------------------------------------------------
   while (!glfwWindowShouldClose(window))
@@ -189,28 +202,37 @@ int main()
     lightShader.setVec3("viewPos", camera.Position);
 
     lightShader.setVec3("light.position", lightPos);
+    // lightShader.setVec3("light.direction", glm::vec3(-0.2f, -1.0f, -0.3f));
     lightShader.setVec3("light.ambient", glm::vec3(0.2f, 0.2f, 0.2f));
     lightShader.setVec3("light.diffuse", glm::vec3(0.5f, 0.5f, 0.5f));
     lightShader.setVec3("light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
+    lightShader.setFloat("light.constant", 1.0f);
+    lightShader.setFloat("light.linear", 0.09f);
+    lightShader.setFloat("light.quadratic", 0.032f);
 
     lightShader.setFloat("material.shininess", 64.0f);
-
 
     glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
     glm::mat4 view = camera.GetViewMatrix();
     lightShader.setMat4("projection", projection);
     lightShader.setMat4("view", view);
 
-    // world transformation
-    glm::mat4 model = glm::mat4(1.0f);
-    lightShader.setMat4("model", model);
-
     glBindVertexArray(VAO);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture);
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, specular);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glm::mat4 model = glm::mat4(1.0f);
+    for (unsigned int i = 0; i < 10; i++)
+    {
+      model = glm::mat4(1.0f);
+      model = glm::translate(model, cubePositions[i]);
+      float angle = 20.0f * i;
+      model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+      lightShader.setMat4("model", model);
+
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
 
     lampShader.use();
     model = glm::mat4(1.0f);
@@ -219,9 +241,6 @@ int main()
     lampShader.setMat4("model", model);
     lampShader.setMat4("view", view);
     lampShader.setMat4("projection", projection);
-    glBindVertexArray(lampVAO);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-
     glBindVertexArray(lampVAO);
     glDrawArrays(GL_TRIANGLES, 0, 36);
 
--- shader/lightShader.fs
+++ shader/lightShader.fs
@@ -13,10 +13,15 @@ struct Material {
 
 struct Light {
   vec3 position;
+  // vec3 direction;
 
   vec3 ambient;
   vec3 diffuse;
   vec3 specular;
+
+  float constant;
+  float linear;
+  float quadratic;
 };
 
 uniform vec3 viewPos;
@@ -25,17 +30,21 @@ uniform Light light;
 
 void main()
 {
-    vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
+    float distance = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * distance * distance);
+
+    vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb * attenuation;
 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
+    // vec3 lightDir = normalize(-light.direction);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb * attenuation;
 
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
+    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb * attenuation;
 
     vec3 result = ambient + diffuse + specular;
 
